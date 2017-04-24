f8026f8e2c1c728de2632a49b53a961b826120c6
Simchollo <simonchollethan@gmail.com>
2017-04-24 22:47:26 +0200

    Pointer

--- Lot_A.c
+++ Lot_A.c
@@ -10,11 +10,9 @@
 #include <time.h>
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \struct plateau
-* \brief On définit le type plateau représenté par une matrice à deux dimensions
-*/
+/* On définit le type plateau représenté par une matrice à deux dimensions */
 /*----------------------------------------------------------------------------------*/
+
 struct plateau{
   int taille;  /* n = taille de la matrice */
   int **contenu; /* Représente le contenu d'une case */
@@ -23,12 +21,9 @@ struct plateau{
 typedef struct plateau plateau;
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn plateau new_plateau(int n)
-* \param Nécessite un entier n >0 qui sera la taille du plateau
-* \return Crée une matrice de taille n ne contenant que des 0 
-* \brief Attribue n^2 cases mémoires pour les cases 
-*/
+/* @requires : Nécessite un entier n >0 qui sera la taille du plateau
+   @assigns : Attribue n^2 cases mémoires pour les cases 
+   @ensures : Crée une matrice de taille n ne contenant que des 0 */
 /*----------------------------------------------------------------------------------*/
 
 plateau new(int n){
@@ -43,14 +38,10 @@ plateau new(int n){
 }
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn plateau aleatoire(int n, int couleurs)
-* \param Nécessite un entier n >0  (taille du plateau), et un nombre de couleurs >0
-* \return Crée un plateau contenant des couleurs aléatoires 
-* \brief Attribue n^2 cases mémoires pour les cases 
-*/
+/* @requires : Nécessite un entier n >0  (taille du plateau), et un nombre de couleurs >0
+   @assigns : Attribue n^2 cases mémoires pour les cases 
+   @ensures : Crée un plateau contenant des couleurs aléatoires */
 /*----------------------------------------------------------------------------------*/
-
 
 plateau aleatoire(int n, int couleurs){
   /* srand(time(NULL));*/
@@ -69,23 +60,19 @@ plateau aleatoire(int n, int couleurs){
 }
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn void changement_couleur(plateau P, int i, int j, int couleur)
-* \param Nécessite un plateau, une case ([i][j]) et une nouvelle couleur
-* \return Rien  
-*/
+/* @requires : Nécessite un plateau, une case ([i][j]) et une nouvelle couleur
+   @assigns : Modifie une case mémoire du plateau
+   @ensures : Rien  */
 /*----------------------------------------------------------------------------------*/
 
-void changement_couleur(plateau P, int i, int j, int couleur){
-  P.contenu[i][j]=couleur;
+void changement_couleur(plateau *P, int i, int j, int couleur){
+  P->contenu[i][j]=couleur;
 }
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn void affiche(plateau P)
-* \param Nécessite un plateau
-* \return Affiche le plateau 
-*/
+/* @requires : Nécessite un plateau
+   @assigns : Rien
+   @ensures : Affiche le plateau */
 /*----------------------------------------------------------------------------------*/
 
 void affiche(plateau P){
@@ -103,44 +90,38 @@ void affiche(plateau P){
 }
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn void supprime(plateau P)
-* \param Nécessite un plateau
-* \return Rien 
-*/
+/* @requires : Nécessite un plateau
+   @assigns : libère l'espace mémoire
+   @ensures : rien */
 /*----------------------------------------------------------------------------------*/
-
-void supprime(plateau P){
-  int n=P.taille;
+void supprime(plateau *P){
+  int n=P->taille;
   int i;
   for (i=0;i<n;i++){
-    free((int*)P.contenu[i]);
+    free((int*)P->contenu[i]);
   }
-  free((int**)P.contenu);
+  free((int**)P->contenu);
 }
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn void sauvegarder(plateau p)
-* \param Nécessite un plateau
-* \return Rien 
-* \brief Sauvegarde le plateau dans un fichier : la première ligne correspond à la taille,
+/* @requires : Nécessite un plateau
+   @assigns : Sauvegarde le plateau dans un fichier : la première ligne correspond à la taille,
 ATTENTION : si une sauvegarde était présente elle sera écrasée, le fichier sera créer si besoin
    le plateau est toujours en mémoire
-*/
+   @ensures : */
 /*----------------------------------------------------------------------------------*/
-void sauvegarder(plateau p){
+void sauvegarder(plateau *p){
   FILE* fi = fopen( "sauvegarde_partie.txt", "w+" );
   if (fi == NULL)
     { printf (" la sauvegarde n'a pas fonctionnée ");}
   else
-    { int n= p.taille;
+    { int n= p->taille;
       fprintf(fi, "%i\n", n); /* on stock la taille du plateau*/
       int i;
       int j;
       for (i=0; i<n; i++){
 	for (j=0;j<n;j++){
-	  fprintf(fi,"%i\n",p.contenu[i][j]); /* on ecrit un caractère par ligne ( ça permettra une lecture plus facile: une ligne correspond à un entier, on a pas à ce soucier des entiers à plusieurs chiffres ) */
+	  fprintf(fi,"%i\n",p->contenu[i][j]); /* on ecrit un caractère par ligne ( ça permettra une lecture plus facile: une ligne correspond à un entier, on a pas à ce soucier des entiers à plusieurs chiffres ) */
 	};
       };  
       fclose(fi);
@@ -149,13 +130,10 @@ void sauvegarder(plateau p){
 
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn plateau reprise(int n, int m)
-* \param De préférence un fichier sauvegarde_partie.txt, s'il n'y en a pas la fonction renvera un plateau aléatoire de taille n avec m couleurs
+/* @requires : de préférence un fichier sauvegarde_partie.txt, s'il n'y en a pas la fonction renvera un plateau aléatoire de taille n avec m couleurs
    Remarque : la sauvegarde doit correspondre a un plateau de taille et de nombre de couleurs inférieur à 100 
-* \return Le plateau enregistré ( si présent ) ou un plateau aléatoire s'il n'y avait pas de fichier ou un probleme 
-* \brief Récupère le plateau sauvegardé dans un fichier sans le modifier   
-*/
+   @assigns : Récupère le plateau sauvegardé dans un fichier sans le modifier
+   @ensures : le plateau enregistré ( si présent ) ou un plateau aléatoire s'il n'y avait pas de fichier ou un probleme */
 /*----------------------------------------------------------------------------------*/
 
 plateau reprise(int n, int m){
@@ -183,16 +161,16 @@ plateau reprise(int n, int m){
 		tmp=atoi(str); 
 		if ((tmp==0) && (*str != '0')){
 		  printf(" Il y a un probleme avec le fichier de sauvegarde"); /* le caractère n'était pas un entier*/
-		  supprime(p); /* on libère la mémoire qu'on occupait car on va renvoyer un autre plateau */
+		  supprime(&p); /* on libère la mémoire qu'on occupait car on va renvoyer un autre plateau */
 		  return aleatoire(n, m);
 		}
 		else {
-		  changement_couleur(p, i, j, tmp);
+		  changement_couleur(&p, i, j, tmp);
 		}
 	      }
 	      else {
 		printf(" Il y a un probleme avec le fichier de sauvegarde");
-		supprime(p); /* on libère la mémoire qu'on occupait car on va renvoyer un autre plateau */
+		supprime(&p); /* on libère la mémoire qu'on occupait car on va renvoyer un autre plateau */
 		return aleatoire(n, m);
 	      }
 	    }
@@ -202,13 +180,10 @@ plateau reprise(int n, int m){
     }
 }
 
-/*----------------------------------------------------------------------------------*/
-/*! 
-* \struct pile
-* \brief On définit la structure pile pour detecter la tache
-*/
-/*----------------------------------------------------------------------------------*/
 
+/*----------------------------------------------------------------------------------*/
+/*structure de pile pour detecter la tache*/
+/*----------------------------------------------------------------------------------*/
 struct pile{
   int taille;  /* taille de la pile : =-1 si la pile est vide  */
   int *x; /* pile des abscisses*/
@@ -218,14 +193,11 @@ struct pile{
 typedef struct pile pile;
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn pile new_pile (int n)
-* \param un entier n>0
-* \return La ("double")pile 
-* \brief Alloue n cases mémoires pour la pile ATTENTION: la pile devra etre de taille suffisemment grande
-*/
+/* @requires : un entier n>0
+   @assigns : Alloue n cases mémoires pour la pile ATTENTION: la pile devra etre de taille suffisemment grande
+   @ensures : La ("double")pile */
 /*----------------------------------------------------------------------------------*/
-pile new_pile (int n){
+pile new_pile(int n){
   pile p;
   p.taille=-1;
   p.x =malloc(sizeof(int) *n);
@@ -234,120 +206,80 @@ pile new_pile (int n){
 }
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn void supprime_pi(pile pi)
-* \param une pile pi
-* \return Rien
-* \brief Libere la memoire d'une pile
-*/
+/* @requires : une pile pi
+   @assigns : libere la memoire d'une pile
+   @ensures : Rien*/
 /*----------------------------------------------------------------------------------*/
-void supprime_pi(pile pi){
-  free((int*)pi.x);
-  free((int*)pi.y);
+void supprime_pi(pile *pi){
+  free((int*)pi->x);
+  free((int*)pi->y);
 }
 
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn void ajout(pile pi, int x, int y)
-* \param une pile pi et 2 entiers correspondants aux coordonées
-* \return Rien
-* \brief Ajoute le couple d'elements et incrémente la taille de la pile
-*/
+/* @requires : une pile pi et 2 entiers correspondants aux coordonées
+   @assigns : ajoute le couple d'element et incrémente la taille de la pile
+   @ensures : Rien*/
 /*----------------------------------------------------------------------------------*/
-
-void ajout(pile pi, int x, int y){
-  pi.taille=pi.taille+1;
-  pi.x[pi.taille]=x;
-  pi.y[pi.taille]=y;
+void ajout(pile *pi, int x, int y){
+  pi->taille=pi->taille+1;
+  pi->x[pi->taille]=x;
+  pi->y[pi->taille]=y;
 }
 	
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn plateau tache(plateau p, plateau t)
-* \param p un plateau correspondant au plateau de jeu et t un autre plateau qui correspond a l'ancienne tache (t[i][j]==1 si ce point appartient a la tache et 0 sinon)
-* \return la nouvelle tache  
-* \brief Alloue la memoire pour une pile puis la detruit; modifie la matrice de la tache
-*/
+/* @requires : p un plateau correspondant au plateau de jeu et t un autre plateau qui correspond un tableau tache vide (t[i][j]==1 si ce point appartient a la tache et 0 sinon). ATTENTION, la tache doit être vide.
+   @assigns : Alloue la memoire pour une pile puis la detruit; modifie la matrice de la tache
+   @ensures : Rien  */
 /*----------------------------------------------------------------------------------*/
-
-plateau tache(plateau p, plateau t){
-  int couleur=p.contenu[0][0];
-  int n=p.taille;
+void tache(plateau *p, plateau *t){
+  int couleur=p->contenu[0][0];
+  int n=p->taille;
   pile pi=new_pile(n*n);
-  ajout(pi,1,1);
+  ajout(&pi,0,0);
   while (pi.taille > -1){
     /* on recupere le premier element de la pile */
-    int abs=pi.x[pi.taille];
+    int ab=pi.x[pi.taille];
     int ord=pi.y[pi.taille];
     pi.taille=pi.taille - 1;
     /* on l'ajoute a la tache */
-    t.contenu[abs][ord]=1;
+    t->contenu[ab][ord]=1;
     /* a droite */
-    if(abs<(n-1))
+    if(ab<(n-1))
       { 
-	if ((couleur==p.contenu[abs+1][ord]) && (t.contenu[abs+1][ord]==0))
-	  { ajout(pi, abs+1, ord);
-	  }
+		  if ((couleur==p->contenu[ab+1][ord]) && (t->contenu[ab+1][ord]==0))
+	  	{ ajout(&pi, ab+1, ord);
+	  	}
       }
     /* a gauche */
-    if(abs>1)
+    if(ab>1)
       { 
-	if ((couleur==p.contenu[abs-1][ord]) && (t.contenu[abs-1][ord]==0))
-	  { ajout(pi, abs-1, ord);
-	  }
+		  if ((couleur==p->contenu[ab-1][ord]) && (t->contenu[ab-1][ord]==0))
+	  	{ ajout(&pi, ab-1, ord);
+	  	}
       }
     /* en haut */
     if(ord>0)
       { 
-	if ((couleur==p.contenu[abs][ord-1]) && (t.contenu[abs][ord-1]==0))
-	  { ajout(pi, abs, ord-1);
-	  }
+		  if ((couleur==p->contenu[ab][ord-1]) && (t->contenu[ab][ord-1]==0))
+	  	{ ajout(&pi, ab, ord-1);
+	  	}
       }
     /* en bas */
     if(ord<(n-1))
       { 
-	if ((couleur==p.contenu[abs][ord+1]) && (t.contenu[abs][ord+1]==0))
-	  { ajout(pi, abs, ord+1);
-	  }
+		  if ((couleur==p->contenu[ab][ord+1]) && (t->contenu[ab][ord+1]==0))
+	  	{ ajout(&pi, ab, ord+1);
+	  	}
       }
   }
-  supprime_pi(pi);
-  return t;
+  supprime_pi(&pi);
 }
 	
+	
 /*----------------------------------------------------------------------------------*/
-/*! 
-* \fn void colorie_tache(plateau p, plateau t, int c )
-* \param Nécessite un plateau de jeu p et une tache (peut importe son contenu) et un int correspondant a la nouvelle couleur
-* \return Rien 
-* \brief Modifie t (recalcule la composante connexe avant changement de couleur); modifie p en changeant la couleur de la composante connexe
-*/
-/*----------------------------------------------------------------------------------*/
-void colorie_tache(plateau p, plateau t, int c ){
-  int n= t.taille;
-  int i;
-  int j;
-  for (j=0; j<n; j++){
-    for (i=0; i<n; i++){
-      t.contenu[i][j]=0;
-    }
-  } /* on a initialisé la tache a une tache vide */
-  t=tache(p,t);
-  for (j=0; j<n; j++){
-    for (i=0; i<n; i++){
-      if(t.contenu[i][j]==1){
-	changement_couleur(p, i, j, c);
-      }
-    }
-  }
-}
-
-/*----------------------------------------------------------------------------------*/
-/*! 
-* \fn int victoire(plateau P)
-* \param Nécessite un plateau
-* \return Renvoie TRUE (=1) si le plateau est d'une couleur unique, FALSE (=0) sinon 
-*/
+/* @requires : Nécessite un plateau
+   @assigns : Rien
+   @ensures : Renvoie TRUE (=1) si le plateau est d'une couleur unique, FALSE (=0) sinon */
 /*----------------------------------------------------------------------------------*/
 
 int victoire(plateau P){
@@ -370,5 +302,26 @@ int victoire(plateau P){
 	return cond;
 }
 
-
-
+/*----------------------------------------------------------------------------------*/
+/* @requires : Nécessite un plateau de jeu p et une tache (peut importe son contenu) et un int correspondant a la nouvelle couleur
+   @assigns : modifie t (recalcule la composante connexe avant changement de couleur); modifie p en changeant la couleur de la composante connexe
+   @ensures :Rien */
+/*----------------------------------------------------------------------------------*/
+void colorie_tache(plateau *p, plateau *t, int c ){
+  int n= t->taille;
+  int i;
+  int j;
+  for (j=0; j<n; j++){
+    for (i=0; i<n; i++){
+      t->contenu[i][j]=0;
+    }
+  } /* on a initialisé la tache a une tache vide */
+  tache(p,t);
+  for (j=0; j<n; j++){
+    for (i=0; i<n; i++){
+      if(t->contenu[i][j]==1){
+	changement_couleur(p, i, j, c);
+      }
+    }
+  }
+}
