0b4e5cce7eb8e0de5318690c0dc451e397cdbfa2
Simchollo <simonchollethan@gmail.com>
2017-04-24 17:52:51 +0200

    Put the main in main.c

--- Lot_A.c
+++ Lot_A.c
@@ -356,39 +356,4 @@ int victoire(plateau P){
 	}
 	return cond;
 }
-	
-	    
 
-/* Pour faire des tests */
-
-int main(){
-	/* TESTS PLATEAU */
-  plateau p_2 = reprise(1,1);
-  affiche(p_2);
-  plateau P=aleatoire(3,4);
-  affiche(P);
-	/* TESTS VOISINS ET COULEURS */
-  coordonnees V=voisins(P,1,1);
-  aff_coord(V);
-  coordonnees C=connexe(P,1,1);
-  aff_coord(C);
-  changement_couleur(P,2,1,18);
-  affiche(P);
-	/* TESTS VICTOIRE */
-  if (victoire(P)) {
-    printf("Gagné !\n");
-  }
-  else {
-    printf("Pas gagné...\n");
-  }
-  /* tableau de 1*1 case, donc sûr de gagner */
-  printf("%s", (victoire(aleatoire(1,6))) ? "Gagné !" : "Pas gagné...");
-	
-	/* TESTS INITIALISATION FICHIER + SAUVEGARDE + SUPPRESSION */
-  sauvegarder(P);
-  supprime(P);
-  affiche(P);/* ca affiche n'importe quoi -> normal car la memoire de P a été libérée */
-  plateau t=reprise(0,0);
-  affiche(t);
-  return(0);
-};
