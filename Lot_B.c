fce75420b26986f6a9446ba909188086ad15e44b
SIMON <simon@MacBook-Air-de-SIMON.local>
2017-04-28 16:32:07 +0200

    Ajout affichage

--- Lot_B.c
+++ Lot_B.c
@@ -44,3 +44,33 @@ void initialisation(int* n, int* tour_max) {
 	}
 	printf("Le plateau est de taille %d.\n", *tour_max);
 }
+
+char couleurs[6] = {'R', 'V', 'B', 'J', 'G', 'M'};
+
+void barre(int n){
+	int i;
+	printf("+");
+	for (i=0; i<n; i++){
+		printf("---+");
+	}
+	printf("\n");
+}
+
+void aff(plateau P){
+	printf("\n\n\n");
+	printf("\n");
+	int i;
+	int j;
+	int n=P.taille;
+	for (i=0; i<n; i++){
+		barre(n);
+		for (j=0; j<n; j++){
+			int a=P.contenu[i][j];
+			printf("|%2c ", couleurs[a]);
+		}
+			printf("|");
+			printf("\n");
+	}
+	barre(n);
+	printf("\n\n\n");
+}
--- Lot_B.h
+++ Lot_B.h
@@ -2,6 +2,8 @@
 #define LOT_B
 
 void initialsation(int *n, int *tour_max);
-
+char couleurs[6];
+void barre(int n);
+void aff(plateau P);
 
 #endif
