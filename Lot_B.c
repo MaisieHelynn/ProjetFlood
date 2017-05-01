bf7690bca732476770e995cef621474288c02dd8
clochette1 <elyne.amsellem@ensiie.fr>
2017-05-01 19:37:18 +0200

    Lot_B_updtate.c
    juste des modifications du texte affiché

--- Lot_B.c
+++ Lot_B.c
@@ -12,16 +12,16 @@
 void initialisation(int* n, int* tour_max) {
 	int ret = 0;
 	*n = -1;
-	printf("Veuillez rentrer la taille du plateau de jeu :\n");
+	printf("Veuillez entrer la taille du plateau de jeu :\n");
 	ret = scanf("%d", n);
 	
 	while (*n < 1 || *n > 100) {
 		if (ret != 1) {
 			while(getchar() != '\n');
-			printf("Ce n'est pas un nombre, veuillez rentrer la taille du plateau de jeu :\n");
+			printf("Ce n'est pas un nombre, veuillez entrer la taille du plateau de jeu :\n");
 		}
 		else {
-			printf("Ce n'est pas un nombre valide, veuillez rentrer un nombre entre 1 et 100 :\n");
+			printf("Ce n'est pas un nombre valide, veuillez entrer un nombre entre 1 et 100 :\n");
 		}
 		ret = scanf("%d", n);
 	}
@@ -29,16 +29,16 @@ void initialisation(int* n, int* tour_max) {
 	
 	ret = 0;
 	*tour_max = -1;
-	printf("Veuillez le nombre de tours maximum :\n");
+	printf("Veuillez entrer le nombre de coups maximum :\n");
 	ret = scanf("%d", tour_max);
 	
 	while (*tour_max < 1 || *tour_max > 1000) {
 		if (ret != 1) {
 			while(getchar() != '\n');
-			printf("Ce n'est pas un nombre, veuillez rentrer le nombre de tours maximum :\n");
+			printf("Ce n'est pas un nombre, veuillez entrer le nombre de coups maximum :\n");
 		}
 		else {
-			printf("Ce n'est pas un nombre valide, veuillez rentrer un nombre entre 1 et 1000 :\n");
+			printf("Ce n'est pas un nombre valide, veuillez entrer un nombre entre 1 et 1000 :\n");
 		}
 		ret = scanf("%d", tour_max);
 	}
